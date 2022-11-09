d9fd2f7627ca5b6530e158b7b1ad01d5fdb3ff31
ray-zero2 <rei2219.matsuda@gmail.com>
2022-11-10 02:48:18 +0900

    make

--- resouces/step9/main.js
+++ resouces/step9/main.js
@@ -2,11 +2,10 @@ import './style.scss'
 import Framer from '@ray-zero2/animation-framer';
 
 const framer = Framer.getInstance();
-// import Gl from './src/gl'
 const $light = document.querySelector('.yurayuraLight');
 const $balls = Array.from(document.querySelectorAll('.ball'));
 const randoms = $balls.map(() => [Math.random(), Math.random(), Math.random()]);
-const radius = 50;
+const radius = Math.max(window.innerWidth * 75/1420, 30);
 
 const update = ({time, deltaTime}) => {
   const _time = time * 2;
@@ -16,16 +15,16 @@ const update = ({time, deltaTime}) => {
     const y = radius * Math.sin(_time  * random[1] + random[2] * 10);
     const scale = 1 + Math.sin(time  * random[2] + random[0] * 50) / 5;
     $ball.style.transform = `matrix(${scale}, 0, 0, ${scale}, ${x}, ${y})`;
+    $ball.style.opacity = Math.random() <= 0.15 ? 0 : 1;
   })
 
   $light.style.transform = `matrix(
     ${1 + 0.1 * Math.sin(Math.random())}, 0,
      0, ${1 + 0.1 * Math.sin(Math.random())},
-     ${Math.random() * 5}, ${Math.random() * 5})`;
+     ${Math.random() * 5 * 3}, ${Math.random() * 5 * 3})`;
 }
 
 const start = async () => {
-  // new Gl();
   framer.add({
     id: 'balls',
     update
