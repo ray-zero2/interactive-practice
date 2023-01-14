1f68a2de3b8de4945a3dbf8a1341798f60bf98de
ray-zero2 <rei2219.matsuda@gmail.com>
2023-01-14 20:58:26 +0900

    build

--- resouces/step10/main.js
+++ resouces/step10/main.js
@@ -1,51 +1,88 @@
 import './style.scss'
+import Framer from '@ray-zero2/animation-framer';
 import * as bodySegmentation from '@tensorflow-models/body-segmentation';
 import '@tensorflow/tfjs-core';
 import '@tensorflow/tfjs-converter';
 import '@tensorflow/tfjs-backend-webgl';
+import HumanDetector from './src/HumanDetector';
+import Mixer from './src/Mixer';
+import Loading from './src/Loading';
 
 
-const app = async () => {
-  const model = bodySegmentation.SupportedModels.MediaPipeSelfieSegmentation;
-  const segmenterConfig = {
-    runtime: 'tfjs',
-    modelType: 'general'
-  }
-  const segmenter = await bodySegmentation.createSegmenter(model, segmenterConfig);
-  const imageElement = document.querySelector('.js-image');
-  console.log(imageElement);
-  // const segmentationConfig = {multiSegmentation: false, segmentBodyParts: falsBe};
-  // const segmentedPeople = await segmenter.segmentPeople(imageElement, segmentationConfig);
-  const segmentedPeople = await segmenter.segmentPeople(imageElement);
-  console.log(segmentedPeople);
- ////////////////
-
-  const foregroundColor = {r: 0, g: 0, b: 0, a: 0};
-  const backgroundColor = {r: 0, g: 0, b: 0, a: 255};
-  const drawContour = false;
-  const foregroundThreshold = 0.3;
-  const backgroundDarkeningMask = await bodySegmentation.toBinaryMask(segmentedPeople, foregroundColor, backgroundColor, drawContour, foregroundThreshold);
-  const opacity = 0.7;
-  const maskBlurAmount = 0; // Number of pixels to blur by.
-  const canvas = document.createElement('canvas');
-  console.log(imageElement.naturalWidth, imageElement.naturalHeight);
-  canvas.width = imageElement.naturalWidth;
-  canvas.height = imageElement.naturalHeight;
+// const app = async () => {
+//   const peopleSegmenter = new HumanDetector();
+//   await peopleSegmenter.init();
+//   const imageElement = document.querySelector('.js-image');
+//   const mask = await peopleSegmenter.getPeopleMask(imageElement);
+//   const opacity = 1;
+//   const maskBlurAmount = 0; // Number of pixels to blur by.
+//   const canvas = document.createElement('canvas');
+//   console.log(imageElement.naturalWidth, imageElement.naturalHeight);
+//   canvas.width = imageElement.naturalWidth;
+//   canvas.height = imageElement.naturalHeight;
   
-  const people = await bodySegmentation.drawMask(canvas, imageElement, backgroundDarkeningMask, backgroundDarkeningMask, opacity, maskBlurAmount);
+//   const people = await bodySegmentation.drawMask(canvas, imageElement, mask, opacity, maskBlurAmount);
 
-//  /////////////
-  // const maskImageData = await people[0].mask.toImageData();
+// //  /////////////
+//   // const maskImageData = await people[0].mask.toImageData();
   
-  // const canvas = document.createElement('canvas');
-  // const ctx = canvas.getContext('2d');
-  // canvas.width = maskImageData.width;
-  // canvas.height = maskImageData.height;
-  // ctx.putImageData(maskImageData, 0, 0 );
-  // ctx.globalAlpha = 0.3;
-  // ctx.drawImage(imageElement, 0, 0, maskImageData.width, maskImageData.height);
-  document.body.append(canvas);
+//   // const canvas = document.createElement('canvas');
+//   // const ctx = canvas.getContext('2d');
+//   // canvas.width = maskImageData.width;
+//   // canvas.height = maskImageData.height;
+//   // ctx.putImageData(maskImageData, 0, 0 );
+//   // ctx.globalAlpha = 0.3;
+//   // ctx.drawImage(imageElement, 0, 0, maskImageData.width, maskImageData.height);
+//   document.body.append(canvas);
+// }
+
+const createPeopleSegmenter = async() => {
+  const peopleSegmenter = new HumanDetector();
+  await peopleSegmenter.init();
+  return peopleSegmenter;
 }
 
+const fetchHumanImage = () => {
+  return new Promise(resolve => {
+    const image = new Image();
+    image.onload = () => {
+      resolve(image);
+    }
 
-window.addEventListener('load' , () => { app() });
+    const randomNum = Math.floor((Math.random() * 9))
+    image.src = `./assets/image_${randomNum}.jpg`;
+  })
+}
+
+const start = async () => {
+  const framer = Framer.getInstance();
+  const peopleSegmenter = await createPeopleSegmenter();
+  const humanImage = await fetchHumanImage();
+  const canvas = document.querySelector('.js-canvas');
+  canvas.width = humanImage.naturalWidth;
+  canvas.height = humanImage.naturalHeight;
+  const mixer = new Mixer(canvas);
+  const mask = await peopleSegmenter.getPeopleMask(humanImage);
+
+
+  // final settings
+  mixer.setTextureImage(mask, 0);
+  mixer.setTextureImage(humanImage, 1);
+
+  const update = ({time, deltaTime}) => {
+    mixer.update(deltaTime);
+    mixer.draw();
+  }
+
+  framer.add({
+    id: 'noise',
+    update
+  })
+  framer.start();
+
+  const loading = new Loading('.js-loading');
+  loading.hide();
+  // mixer.setTextureImage(humanImage, 0)
+}
+
+window.addEventListener('load' , () => { start() });
