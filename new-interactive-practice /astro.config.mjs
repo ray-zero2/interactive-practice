8505c3f2b66989fd78fa518d558bc7b36cfe1440
ray-zero2 <rei2219.matsuda@gmail.com>
2023-04-10 16:54:26 +0900

    wip

--- new-interactive-practice /astro.config.mjs
+++ new-interactive-practice /astro.config.mjs
@@ -1,13 +1,18 @@
 import {
   defineConfig
 } from 'astro/config';
+import glsl from 'vite-plugin-glsl'
 
 // https://astro.build/config
 export default defineConfig( {
   outDir: '../docs/',
+  "paths": {
+    "@scripts/*": ["src/scripts/*"],
+  },
   vite: {
     build: {
       emptyOutDir: false
-    }
+    },
+    plugins: [glsl()]
   }
 } );
---
+++ new-interactive-practice /src/scripts/step12/src/GlContent/lights/directionalLight/index.ts
@@ -0,0 +1,56 @@
+import * as THREE from 'three';
+import gui from '../../utils/gui'
+
+export default class SpotLight extends THREE.SpotLight {
+  constructor(color = "#ffffff") {
+    super(color, 2);
+    this.castShadow = true;
+    this.angle = 20;
+    this.penumbra = 0.4;
+    this.decay = 2;
+    this.distance = 50;
+    this._helper = new THREE.SpotLightHelper( this );
+    this.time = 0;
+    this.position.set(
+      Math.floor(Math.random() * 10),
+      Math.floor(Math.random() * 10),
+      Math.floor(Math.random() * 10) );
+    this.init();
+    this.setGui();
+  }
+
+  init() {
+  }
+
+  getHelper() {
+    return this._helper;
+  }
+
+  update(deltaTime) {
+    this.time += deltaTime;
+    this._helper.update();
+  }
+
+
+  setGui() {
+    const folder = gui.addFolder(`spotlight - ${this.id}`)
+    folder.add(this._helper, 'visible');
+    folder.add(this, 'angle').min(0).max(Math.PI/2).step(Math.PI/90);
+    folder.add(this, 'distance').min(1).max(180).step(1);
+    folder.add(this.position, 'x').min(-50).max(50).step(1);
+    folder.add(this.position, 'y').min(-50).max(50).step(1);
+    folder.add(this.position, 'z').min(-50).max(50).step(1);
+    folder.add(this, 'penumbra').min(0).max(1).step(0.01);
+    folder.add(this, 'decay').min(0).max(5).step(0.01);;
+      // .onChange(() => {
+      //   this.lookAt(new THREE.Vector3(0, 0, 0));
+      //   this._helper.update();
+      // });
+    // folder.add(dofEffect, 'bokehScale').min(0).max(20).step(0.5)
+    // folder
+    //   .add(dofEffect.circleOfConfusionMaterial, 'focalLength')
+    //   .min(0.01)
+    //   .max(0.15)
+    //   .step(0.01)
+  }
+}
