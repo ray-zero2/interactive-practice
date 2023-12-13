0b1bde6115743ac62cad37314aa6525b6d8f1359
Rei Matsuda <rei2219.matsuda@gmail.com>
2023-12-14 01:42:25 +0900

    add 16

---
+++ new-interactive-practice/src/components/step16/script/index.ts
@@ -0,0 +1,44 @@
+import { MusicPlayer } from '@src/components/step16/script/src/MusicPlayer/MusicPlayer';
+import GlContent from './src/GlContent/index';
+import gsap from 'gsap';
+
+const modal = document.querySelector('.js-modal')!;
+const startButton = document.querySelector('.js-button[data-type="start"]')!;
+
+const playButton = document.querySelector('.js-button[data-type="play"]')!;
+const stopButton = document.querySelector('.js-button[data-type="stop"]')!;
+const volume = document.querySelector('.js-volume')!;
+
+
+let content: GlContent | null = null;
+let player: MusicPlayer | null = null;
+
+window.addEventListener('load', () => {
+  content = new GlContent(document.querySelector('.canvas')!);
+})
+
+startButton!.addEventListener('click', () => {
+  player = new MusicPlayer(40, 72.5);
+  player.loadAudioFile('techno.mp3').then(() => player?.play());
+  gsap.to(modal, { autoAlpha: 0, duration: 0.5});
+
+  player.on('beat', () => {
+    content!.beated()
+  });
+}, { once: true });
+
+
+playButton!.addEventListener('click', () => {
+  if(player !== null) player.play();
+}, { passive: true });
+
+stopButton!.addEventListener('click', () => {
+  if(player !== null) player.stop();
+}, { passive: true });
+
+volume!.addEventListener('input', (e) => {
+  const volumeValue = parseInt((e.target as HTMLInputElement).value, 10);
+  const normalizedVolume = Math.max(Math.min(100, volumeValue),0) / 100;
+  if(player !== null) player.setVolume(normalizedVolume);
+}, { passive: true })
+
